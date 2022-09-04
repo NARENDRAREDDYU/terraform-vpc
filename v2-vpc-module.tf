f0ceabe70f4a6d96002e354bc4113bce40341042
NARENDRAREDDYU <108210882+NARENDRAREDDYU@users.noreply.github.com>
2022-09-04 07:24:48 +0530

    output block added

--- v2-vpc-module.tf
+++ v2-vpc-module.tf
@@ -174,4 +174,34 @@ variable "single_nat_gateway" {
 }
 
 
+# Output Block
+
+output "cidr" {
+  description = "vpc_cidr block"
+  value = module.vpc.vpc_id
+
+}
+
+output "public_subnets" {
+  description = "Public_subnets"
+  value = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "private_subnets"
+  value = module.vpc.private_subnets
+  
+}
+
+output "nat_public_ips" {
+  description = "nat_public_ips"
+  value = module.vpc.nat_public_ips
+}
+
+output "azs" {
+  description = "availability zones"
+  value = module.vpc.azs
+  
+}
+
 
